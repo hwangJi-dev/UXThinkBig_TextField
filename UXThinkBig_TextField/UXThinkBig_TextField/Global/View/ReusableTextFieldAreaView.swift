f69313415d621416bf373ca7e8f04d18cea30edb
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-11 00:35:44 +0900

    ➕[ADD] : 재사용 뷰 xib, 클래스파일 추가

---
+++ UXThinkBig_TextField/UXThinkBig_TextField/Global/View/ReusableTextFieldAreaView.swift
@@ -0,0 +1,20 @@
+//
+//  ReusableTextFieldAreaView.swift
+//  UXThinkBig_TextField
+//
+//  Created by 황지은 on 2021/10/11.
+//
+
+import UIKit
+
+class ReusableTextFieldAreaView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
