0113dca9200932e1ca826b80939c065e5eea69fd
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-11 05:38:52 +0900

    ➕[ADD] : Notification.Name Extension 추가

---
+++ UXThinkBig_TextField/UXThinkBig_TextField/Global/Extension/Notification.Name.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name.swift
+//  UXThinkBig_TextField
+//
+//  Created by 황지은 on 2021/10/11.
+//
+
+import Foundation
+extension Notification.Name {
+    // 키보드 엔터키 눌렀을 때
+    static let keyboardEnterClicked = Notification.Name("keyboardEnterClicked")
+}
